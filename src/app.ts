3617c9c5330c4ee0a87f34fde4287cbc9ffc51cb
jefeez <jefersonquez@gmail.com>
2024-08-26 20:40:10 -0300

    envs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
 import { env } from './env'
 
+console.log('ENV:', env.DB_USER, env.DB_PASSWORD, env.DB_DATABASE)
 export const app = () => `node.ts / ${env.NODE_ENV}`
--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 const schemas = z.object({
   NODE_ENV: z.string().default('dev'),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_DATABASE: z.string(),
 })
 
 export const env = schemas.parse(process.env)
